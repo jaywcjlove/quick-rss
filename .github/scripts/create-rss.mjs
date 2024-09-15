df604b47a3c2e72978064bd741d5993544655758
jaywcjlove <398188662@qq.com>
2024-09-16 01:33:43 +0800

    chore: update issue template scripts

--- .github/scripts/create-rss.mjs
+++ .github/scripts/create-rss.mjs
@@ -156,12 +156,12 @@ const rssFilePath = `./docs/rss/${year}-${week}.json`;
       })
     })
 
-    const markdownContent = fs.readFileSync('./docs/README.md', 'utf-8');
+    const markdownContent = fs.readFileSync('./docs/feed.md', 'utf-8');
     // <!--RSS_LIST_START--><!--RSS_LIST_END-->
     const contentx = markdownContent.replace(/<!--RSS_LIST_START-->[\s\S]*<!--RSS_LIST_END-->/g, `<!--RSS_LIST_START-->\n${mdListContent}\n<!--RSS_LIST_END-->`);
     fs.writeFileSync('./docs/feed.md', contentx);
     // 输出写入成功日志
-    info(`README.md 文件写入成功：./docs/README.md`);
+    info(`feed.md 文件写入成功：./docs/feed.md`);
 
     const jsonFeedPath = './docs/feed.json';
     await fs.writeFile(jsonFeedPath, feed.json1());
